398589955d7e9562a6431bd1191be934df00841c
gpl <gplperfectlife@163.com>
2024-09-02 17:39:52 +0800

    del

--- PracticeMakesPerfect/C/string/atoi.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    char a[]="123";
-    a[0]=atoi(a);
-    printf("%d",a[0]);
-    return 0;
-}
--- PracticeMakesPerfect/C/string/strchr.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char a[]="hello";
-    char*p=strstr(a,"e");
-    printf("%s",p);
-    return 0;
-}
